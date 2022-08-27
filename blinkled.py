99f2bd1a0bde2ec402fe15d1a3f44fc77b7cc11c
errorsxp <59858550+errorsxp@users.noreply.github.com>
2022-08-27 14:29:01 -0400

    Add files via upload

---
+++ blinkled.py
@@ -0,0 +1,12 @@
+import time
+import board
+import digitalio
+
+led = digitalio.DigitalInOut(board.LED)
+led.switch_to_output()
+
+while True:
+    led.value = True
+    time.sleep(0.1)
+    led.value = False
+    time.sleep(0.1)
